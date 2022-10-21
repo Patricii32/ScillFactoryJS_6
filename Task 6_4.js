fd850e87f95dad533b9e35ebbbdea8b16d7f86f0
Denis Kravtsov <76997556+Patricii32@users.noreply.github.com>
2022-10-21 23:08:09 +0300

    Update Task 6_4.js
    Version 2.0 added new solution

--- Task 6_4.js
+++ Task 6_4.js
@@ -1,3 +1,4 @@
+//Решение с помощью SetInterval
 const func = function (a,b){
 	timerId = setInterval(function(){
 		if( a>b){
@@ -9,3 +10,16 @@ const func = function (a,b){
    	}, 1000);
 }
 func(5,15)
+
+/* Альтернативное решение с помощью setTimeout*/
+function func(a, b) {
+	let current = a;
+	setTimeout(function start() {
+		console.log(current);
+		if (current < b) {
+			setTimeout(start, 1000);
+		}
+		current++;
+	}, 1000);
+}
+func(5, 15);
